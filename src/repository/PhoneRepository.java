61dbf9f907aae6f42516f837162fd728fe94a2a9
George Skoufias <gskoufias@cognity.gr>
2019-07-24 18:21:35 +0300

    Exceptions Repos and Fault Codes

--- src/repository/PhoneRepository.java
+++ src/repository/PhoneRepository.java
@@ -1,4 +1,37 @@
 package repository;
 
+import model.PhoneRecord;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PhoneRepository {
+
+	//	CRUD OPERATIONS
+	//	CRUD OPERATIONS
+	//	CRUD OPERATIONS
+
+	//	Create
+
+	List<PhoneRecord> addPhones(List<PhoneRecord> addThesePhones);
+
+	List<PhoneRecord> addOnePhone(PhoneRecord addPhones);
+
+	//	Read
+
+	List<PhoneRecord> getAllPhones(String phoneRecordFile);
+
+	Optional<PhoneRecord> getOnePhone(int recordId);
+
+	Optional<PhoneRecord> getOneSpecificPhoneRecord(String name, String surName, String number);
+
+
+	//	Update
+
+	PhoneRecord changePhoneRecord(PhoneRecord newRecord);
+
+	//	Delete
+
+	void killPhoneRecord(int recordId);
+
 }

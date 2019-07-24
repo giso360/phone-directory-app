565b6ec28d5c9daf685397a081df8405d9a0d90d
George Skoufias <gskoufias@cognity.gr>
2019-07-24 17:42:03 +0300

    Text Data

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
+
+        File file = new File("data/phoneDirectory.txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        //Read lines one by one
+
+        String line = bufferedReader.readLine();
+        int countLines = 1;
+        while (line != null){
+            System.out.println("line#" + countLines + " : " + line);
+            countLines += 1;
+            line = bufferedReader.readLine();
+
+        }
+        bufferedReader.close();
+
     }
+
+
 }
